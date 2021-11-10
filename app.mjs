a1e1e719e1bd55c8bd56a8fbce946cabcb8ce72c
Hamza Ilyas <hamza24061@gmail.com>
2021-11-10 18:35:43 +0500

    code change

--- app.mjs
+++ app.mjs
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 const DB =
   "mongodb+srv://hamzailyas:db1234@cluster0.8m2bm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-  mongoose.connect(DB)
+mongoose.connect(DB);
 
 const User = mongoose.model("User", {
   name: String,
@@ -14,99 +14,94 @@ const User = mongoose.model("User", {
   address: String,
 });
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.use(cors());
-
-app.use(morgan("short"));
+app.use(cors())
+app.use(express.json())
+app.use(morgan('short'))
 
 app.use((req, res, next) => {
-  console.log("Request Received", req.body);
-  next();
-});
+  console.log("a request came", req.body);
+  next()
+})
 
-app.get("/", (req, res) => {
-  res.send("Welcome User!");
-});
+app.get('/users', (req, res) => {
 
-app.get("/users", (req, res) => {
   User.find({}, (err, users) => {
     if (!err) {
-      res.send(users);
+      res.send(users)
     } else {
-      res.status(500).send("error happened!");
+      res.status(500).send("error happened")
     }
-  });
-});
+  })
 
-app.post("/user", (req, res) => {
+
+})
+app.get('/user/:id', (req, res) => {
+
+  User.findOne({ _id: req.params.id }, (err, user) => {
+    if (!err) {
+      res.send(user)
+    } else {
+      res.status(500).send("error happened")
+    }
+  })
+
+})
+app.post('/user', (req, res) => {
+
   if (!req.body.name || !req.body.email || !req.body.address) {
-    res.status(400).send("Invalid Data");
+    res.status(400).send("invalid data");
   } else {
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
-      address: req.body.address,
+      address: req.body.address
     });
-
     newUser.save().then(() => {
-      console.log("User Created Successfully");
-      res.send("User Created");
+      console.log('user created success')
+      res.send("users created");
     });
   }
-});
-
-app.get("/user/:id", (req, res) => {
-  User.findOne({ _id: req.params.id }, (err, user) => {
-    if (!err) {
-      res.send(user);
-    } else {
-      res.status(500).send("error happened!");
-    }
-  });
-});
-
-app.put("/user/:id", (req, res) => {
-  let updateObj = {};
+})
+app.put('/user/:id', (req, res) => {
+  let updateObj = {}
 
   if (req.body.name) {
-    updateObj.name = req.body.name;
+    updateObj.name = req.body.name
   }
   if (req.body.email) {
-    updateObj.email = req.body.email;
+    updateObj.email = req.body.email
   }
   if (req.body.address) {
-    updateObj.address = req.body.address;
+    updateObj.address = req.body.address
   }
 
-  User.findByIdAndUpdate(
-    req.params.id,
-    updateObj,
-    { new: true },
+  User.findByIdAndUpdate(req.params.id, updateObj, { new: true },
     (err, data) => {
       if (!err) {
-        res.send(data);
+        res.send(data)
       } else {
-        res.status(500).send("error happened!");
+        res.status(500).send("error happened")
       }
-    }
-  );
-});
+    })
+})
+app.delete('/user/:id', (req, res) => {
 
-app.delete("/user/:id", (req, res) => {
   User.findByIdAndRemove(req.params.id, (err, data) => {
     if (!err) {
-      res.send("User Deleted");
+      res.send("user deleted")
     } else {
-      res.status(500).send("error happened!");
+      res.status(500).send("error happened")
     }
-  });
-});
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send('Hi I am a hello world Server program')
+})
 
 app.listen(port, () => {
-  console.log(`Server Started On Port ${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
