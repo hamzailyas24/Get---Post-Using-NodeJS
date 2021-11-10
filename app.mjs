39a273f65614086428186d8bc37b7ecb54319a53
Hamza Ilyas <hamza24061@gmail.com>
2021-11-10 18:19:57 +0500

    Mongo Connected

--- app.mjs
+++ app.mjs
@@ -1,12 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+
+const DB =
+  "mongodb+srv://hamzailyas:db1234@cluster0.8m2bm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  address: String,
+});
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-
-let users = [];
 
 app.use(express.json());
 
@@ -19,64 +27,82 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("Request Received");
-  next();
-});
-
 app.get("/", (req, res) => {
   res.send("Welcome User!");
 });
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  User.find({}, (err, users) => {
+    if (!err) {
+      res.send(users);
+    } else {
+      res.status(500).send("error happened!");
+    }
+  });
 });
 
 app.post("/user", (req, res) => {
   if (!req.body.name || !req.body.email || !req.body.address) {
     res.status(400).send("Invalid Data");
   } else {
-    users.push({
+    const newUser = new User({
       name: req.body.name,
       email: req.body.email,
       address: req.body.address,
     });
-    res.send("User Created");
+
+    newUser.save().then(() => {
+      console.log("User Created Successfully");
+      res.send("User Created");
+    });
   }
 });
 
 app.get("/user/:id", (req, res) => {
-  if (users[req.params.id]) {
-    res.send(users[req.params.id]);
-  } else {
-    res.send("User Not Found");
-  }
+  User.findOne({ _id: req.params.id }, (err, user) => {
+    if (!err) {
+      res.send(user);
+    } else {
+      res.status(500).send("error happened!");
+    }
+  });
 });
 
 app.put("/user/:id", (req, res) => {
-  if (users[req.params.id]) {
-    if (req.body.name) {
-      users[req.params.id].name = req.body.name;
-    }
-    if (req.body.name) {
-      users[req.params.id].email = req.body.email;
-    }
-    if (req.body.name) {
-      users[req.params.id].email = req.body.email;
-    }
-    res.send(users[req.params.id]);
-  } else {
-    res.send("User Not Found");
+  let updateObj = {};
+
+  if (req.body.name) {
+    updateObj.name = req.body.name;
   }
+  if (req.body.email) {
+    updateObj.email = req.body.email;
+  }
+  if (req.body.address) {
+    updateObj.address = req.body.address;
+  }
+
+  User.findByIdAndUpdate(
+    req.params.id,
+    updateObj,
+    { new: true },
+    (err, data) => {
+      if (!err) {
+        res.send(data);
+      } else {
+        res.status(500).send("error happened!");
+      }
+    }
+  );
 });
 
 app.delete("/user/:id", (req, res) => {
-  if (users[req.params.id]) {
-    users[req.params.id] = {};
-    res.send("User Deleted");
-  } else {
-    res.send("User Not Found");
-  }
+  User.findByIdAndRemove(req.params.id, (err, data) => {
+    if (!err) {
+      res.send("User Deleted");
+    } else {
+      res.status(500).send("error happened!");
+    }
+  });
 });
 
 app.listen(port, () => {
